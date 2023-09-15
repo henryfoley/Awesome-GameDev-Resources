4bbf8692b382bf7fc70c537f708c4325b8ef982c
Henry F <henry.foley@mymail.champlain.edu>
2023-09-15 14:18:43 -0400

    Life Function test

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
 int main(){
+
+  cout<< "Hello World" << endl;
+
+  int rows;
+  int columns;
+  int steps;
+
+  //Number of Rows
+  //Number of Columns
+  //
   return 0;
+
+  //Implement double buffer
 };
