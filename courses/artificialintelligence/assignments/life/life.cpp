136d445e6ed19f33b61d7305a54688d2aff2e370
Henry F <henry.foley@mymail.champlain.edu>
2023-09-29 14:27:35 -0400

    Started on GOL approach

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
-  cout<< "Hello World" << endl;
+  vector<char> vertical, horizontal;
+  char currentPoint;
 
-  int rows;
-  int columns;
-  int steps;
+  int rows = 0;
+  int columns = 0;
+  int steps = 0;
 
   //Number of Rows
   //Number of Columns
-  //
+  cin >> rows >> columns >> steps;
+  cout << "Num Rows " << rows << endl;
+  cout << "Num Columns " << columns << endl;
+  cout << "Num Steps " << steps << endl;
+
+  // Init Board
+  vector<vector<char>> board(columns, vector<char>(rows));
+
+  // Get World
+  for (int i = 0; i<rows; i++)
+  {
+    cin >> currentPoint;
+    for(int j = 0; j<columns; j++)
+    {
+      //input each point in row
+      //Use getline
+      cin >> currentPoint;
+      board[i][j] = currentPoint;
+    }
+  }
+
+  cout << "Board Size: " << board.size();
+
   return 0;
 
   //Implement double buffer
